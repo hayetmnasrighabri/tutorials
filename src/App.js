1c9e555c11064b7c3cfc1137413d3447a4ce40e9
hayetmnasri <hayetmnasri@hotmail.fr>
2024-10-03 17:06:30 +0200

    Les hooks personnalisées

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [name, setName]=useState('gedeon')
-  setTimeout(()=>{
-    setName('pasdenom')
-  },2000)
+const {name}=useApp()
   return (
     <div>
       {name}
     </div>
   )
 }
-
+const useApp=()=>{
+  const [name, setName]=useState('gedeon')
+  setTimeout(()=>{
+    setName('pasdenom')
+  },2000)
+  return(
+  {name}
+)}
 export default App
